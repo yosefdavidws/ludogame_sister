49e4300efe9176d483757758807871b37f8594d1
Graciela Fausten Novindri <68045746+gfnovi@users.noreply.github.com>
2021-03-15 20:28:10 +0700

    Update Layout.java

--- src/coba_ludo/Layout.java
+++ src/coba_ludo/Layout.java
@@ -16,14 +16,15 @@ public class Layout extends JPanel{
             b1 = b;
             c1 = c;
             d1 = d;
-		x=xi;//250
-		y=yi;//100
+		x=xi;//80
+		y=yi;//50
 		width=30;//lebar kotak satuan (1)
 		height=30;//tinggi kotak satuan (1)
                 
 	}
        
 	public void draw(Graphics2D g){
+            //1
 		g.setColor(Color.WHITE);
                 g.fillRect(x,y,15*width,15*height);//lapisan bawah warna putih (80,50,450,450)
         for(int i=0;i<6;i++) {//warna kotak dalam rumah
@@ -48,6 +49,7 @@ public class Layout extends JPanel{
         	g.fillRect(x+(i*width),y+(14*height), width, height);//bawah
         	g.fillRect(x+(5*width), y+((i+9)*height), width, height);//kanan
         }
+        //2
         for(int i=1;i<6;i++) {//jalan masuk ke tengah
         	g.setColor(Color.RED);
         	g.fillRect(x+(i*width),y+(7*height), width, height);
@@ -67,6 +69,7 @@ public class Layout extends JPanel{
     	g.fillRect(x+(8*width),y+(1*height), width, height);
     	g.setColor(Color.BLUE);
     	g.fillRect(x+((6)*width),y+((13)*height), width, height);
+        //3
     	int x2=x+45,y2=y+45;
     	for(int i=0;i<2;i++) {//tempat player
     		for(int j=0;j<2;j++) {
@@ -101,14 +104,15 @@ public class Layout extends JPanel{
         int ypoints3[] = {y+(9*height),y+(9*height),y+15+(7*width)};
         int npoints3= 3;
         g.fillPolygon(xpoints3, ypoints3, npoints3);
+        //4
     	g.setStroke(new BasicStroke(2));//tebal garis 
-    	g.setColor(Color.BLACK);//memberi garis pinggir
+    	g.setColor(Color.BLACK);//memberi garis kotak-kotak
     	for(int i=0;i<3;i++) {
     		for(int j=0;j<6;j++) {
-    	        g.drawRect(x+((i+6)*width),y+(j*height), width, height);
-    	        g.drawRect(x+((j)*width),y+((i+6)*height), width, height);
-    	        g.drawRect(x+((i+6)*width),y+((j+9)*height), width, height);
-    	        g.drawRect(x+((j+9)*width),y+((i+6)*height), width, height);
+    	        g.drawRect(x+((i+6)*width),y+(j*height), width, height);// atas
+    	        g.drawRect(x+((j)*width),y+((i+6)*height), width, height);//kiri
+    	        g.drawRect(x+((i+6)*width),y+((j+9)*height), width, height);//bawah
+    	        g.drawRect(x+((j+9)*width),y+((i+6)*height), width, height);//kanan
     		}
     	}
     	g.drawRect(x+((1)*width),y+(1*height),4*width,4*height);//garis rumah merah
@@ -133,22 +137,32 @@ public class Layout extends JPanel{
         g.drawOval(x+5+(12*width),y+5+(6*height),width-10,height-10);
         g.drawOval(x+5+(8*width),y+5+(12*height),width-10,height-10);
         g.drawOval(x+5+(2*width),y+5+(8*height),width-10,height-10);
-        g.setFont(new Font("serif", Font.BOLD, 20));//font
-        g.drawString(""+a1, 260, 135);
-        g.drawString(""+b1, 540, 135);
-        g.drawString(""+c1, 260, 620);
-        g.drawString(""+d1, 540, 620);
-		
-	g.setColor(Color.WHITE);//tempat pergantian player
-        g.fillRect(250,5,450,130);
-        g.setColor(Color.BLACK);
-        g.drawString("Mulai", 455, 110);
-        g.setColor(Color.WHITE);
-        g.drawRect(455,20,50,50); 
-        g.setColor(Color.BLACK);//garis
-        g.drawRect(455, 20, 50, 50);
-        g.setColor(Color.BLACK);
-        g.drawString("Dadu", 457, 50);
+        g.setFont(new Font("serif", Font.BOLD, 30));//font
+        g.setColor(Color.RED);
+        g.fillOval(220,623,25,25);
+        g.drawString(""+a1, 250, 646);
         
+        g.setColor(Color.GREEN);
+        g.fillOval(380,623,25,25);
+        g.drawString(""+b1, 410, 646);
+        
+        g.setColor(Color.BLUE);
+        g.fillOval(540,623,25,25);
+        g.drawString(""+c1, 570, 646);
+        
+        g.setColor(Color.YELLOW);
+        g.fillOval(700,623,25,25);
+        g.drawString(""+d1, 730, 646);
+        
+        g.setColor(Color.WHITE);//tempat pergantian player
+    		g.fillRect(250,5,450,130);
+                  g.setColor(Color.WHITE);
+            g.fillRect(455,20,50,50);
+            g.setColor(Color.BLACK);//memberi garis pinggir
+        g.drawRect(455,20,50,50);
+        g.setFont(new Font("serif", Font.BOLD, 15));
+        g.drawString("dadu",465,50);
+        g.setFont(new Font("serif", Font.BOLD, 30));
+        g.drawString("Enter untuk mulai", 370, 110);
     }
 }
